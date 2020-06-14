95e756fa36d0f927a3fdf91114e67dd33ffcc4c5
Michael Aerni <hello@michaelaerni.ch>
2020-06-14 17:51:19 +0200

    Add Utils class

---
+++ src/Utils.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aerni\Factory;
+
+class Utils
+{
+    /**
+     * Recursively map an array to a callback function.
+     *
+     * @param array $array
+     * @param function $callback
+     * @return array
+     */
+    public static function array_map_recursive(array $array, $callback): array
+    {
+        $output = [];
+
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $output[$key] = self::array_map_recursive($value, $callback);
+            } else {
+                $output[$key] = $callback($value, $key);
+            }
+        }
+
+        return $output;
+    }
+}
