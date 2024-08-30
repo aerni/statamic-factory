6e82f85ce8ab78e89f8c25840bcbb8787f167fe4
aerni <aerni@users.noreply.github.com>
2024-08-30 15:16:36 +0000

    Fix styling

--- src/Factories/Factory.php
+++ src/Factories/Factory.php
@@ -5,18 +5,17 @@ namespace Aerni\Factory\Factories;
 use Closure;
 use Exception;
 use Faker\Generator;
-use Statamic\Facades\Site;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use Illuminate\Support\Collection;
 use Illuminate\Container\Container;
-use Statamic\Contracts\Entries\Entry;
-use Illuminate\Support\Facades\Config;
-use Statamic\Contracts\Taxonomies\Term;
-use Illuminate\Support\Traits\Macroable;
-use Illuminate\Support\Traits\Conditionable;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Eloquent\Factories\CrossJoinSequence;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
+use Statamic\Contracts\Entries\Entry;
+use Statamic\Contracts\Taxonomies\Term;
+use Statamic\Facades\Site;
 
 abstract class Factory
 {
